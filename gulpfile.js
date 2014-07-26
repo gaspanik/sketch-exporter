b4ca7c210b2239804ab37ead8de04e16fd5c4ef3
cipher <gmgateway@gmail.com>
2014-07-26 10:03:43 +0900

    0.0.3
    Add imagemin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
 var gulp = require('gulp'),
   shell = require('gulp-shell'),
+  imagemin = require('gulp-imagemin'),
+  runSequence = require('run-sequence'),
   browserSync = require('browser-sync');
 
 var imgsrc = './src/*.sketch';
-var imgdir = './images/';
-
-gulp.task('sketch', function () {
-  return gulp.src(imgsrc,  {read: false})
-    .pipe(shell([
-      'sketchtool export slices --output="' + imgdir + '" <%= file.path %>'
-    ]))
-    .pipe(browserSync.reload({stream:true}));
-});
+var tmpdir = './tmp/';
+var imgdir = './dist/images/';
 
 gulp.task('bs', function() {
   browserSync.init(null, {
@@ -21,8 +16,31 @@ gulp.task('bs', function() {
   });
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./src/**', ['sketch']);
+gulp.task('sketch', function () {
+  return gulp.src(imgsrc,  {read: false})
+    .pipe(shell([
+      'sketchtool export slices --output="' + tmpdir + '" <%= file.path %>'
+    ]))
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('default', ['bs', 'sketch', 'watch']);
+gulp.task('optim', function() {
+  return gulp.src(tmpdir + '*')
+    .pipe(imagemin({
+      optimizationLevel: 3
+    })) // See gulp-imagemin page.
+    .pipe(gulp.dest(imgdir));
+});
+
+gulp.task('export', function() {
+  runSequence(
+    'sketch',
+    'optim'
+  );
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**', ['export']);
+});
+
+gulp.task('default', ['bs', 'export', 'watch']);
